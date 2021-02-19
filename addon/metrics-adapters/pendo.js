34105e04aea768ec712b90f9472adcce29ccac7d
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2021-02-20 00:15:18 +0100

    fix: tests failing because assert import is not done

--- addon/metrics-adapters/pendo.js
+++ addon/metrics-adapters/pendo.js
@@ -13,12 +13,12 @@ export default BaseAdapter.extend({
     let { apiKey } = config
 
     assert(
-      `[ember-metrics] You must pass a valid \`apiKey\` to the ${this.toString()} adapter`,
+      `[ember-metrics-pendo] You must pass a valid \`apiKey\` to the ${this.toString()} adapter`,
       apiKey
     )
 
     /* eslint-disable */
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
+    !(function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
     v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
     o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
     y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${apiKey}/pendo.js`;
