3cbe84520f119345b0d81d6922b29e22a22810d1
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2021-02-20 00:31:53 +0100

    refactor: use native class

--- addon/metrics-adapters/pendo.js
+++ addon/metrics-adapters/pendo.js
@@ -1,12 +1,14 @@
 import { assign } from "@ember/polyfills"
 import { assert } from "@ember/debug"
 import removeFromDOM from "ember-metrics/utils/remove-from-dom"
+import classic from "ember-classic-decorator"
 import BaseAdapter from "ember-metrics/metrics-adapters/base"
 
-export default BaseAdapter.extend({
+@classic
+export default class PendoAdapter extends BaseAdapter {
   toStringExtension() {
     return "Pendo"
-  },
+  }
 
   init() {
     let config = assign({}, this.config)
@@ -24,20 +26,20 @@ export default BaseAdapter.extend({
     y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${apiKey}/pendo.js`;
     z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
     /* eslint-enable */
-  },
+  }
 
   identify(options = {}) {
     window.pendo.initialize(options)
-  },
+  }
 
   trackEvent(options = {}) {
     const { event } = options
     delete options.event
     window.pendo.track(event, options)
-  },
+  }
 
   willDestroy() {
     removeFromDOM('script[src*="pendo.js"]')
     delete window.pendo
   }
-})
+}
