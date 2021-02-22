70c39923132f2dcb3389487c811d18ee5bb0623c
Nathanaël Dekeister <nathanael_dekeister@ultimatesoftware.com>
2021-02-22 14:38:25 +0100

    build: update ember metrics (#72)
    * build: use last version of ember-metrics
    
    * refactor: remove use of canUseDOM
    
    See https://github.com/adopted-ember-addons/ember-metrics/pull/175
    
    * refactor: use removeFromDOM util
    
    * fix: tests failing because assert import is not done
    
    * refactor: use native class

--- addon/metrics-adapters/pendo.js
+++ addon/metrics-adapters/pendo.js
@@ -1,51 +1,45 @@
 import { assign } from "@ember/polyfills"
 import { assert } from "@ember/debug"
-import canUseDOM from "ember-metrics/utils/can-use-dom"
+import removeFromDOM from "ember-metrics/utils/remove-from-dom"
+import classic from "ember-classic-decorator"
 import BaseAdapter from "ember-metrics/metrics-adapters/base"
 
-export default BaseAdapter.extend({
+@classic
+export default class PendoAdapter extends BaseAdapter {
   toStringExtension() {
     return "Pendo"
-  },
+  }
 
   init() {
     let config = assign({}, this.config)
     let { apiKey } = config
 
     assert(
-      `[ember-metrics] You must pass a valid \`apiKey\` to the ${this.toString()} adapter`,
+      `[ember-metrics-pendo] You must pass a valid \`apiKey\` to the ${this.toString()} adapter`,
       apiKey
     )
 
-    if (canUseDOM) {
-      /* eslint-disable */
-      (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
-      v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
-      o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-      y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${apiKey}/pendo.js`;
-      z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-      /* eslint-enable */
-    }
-  },
+    /* eslint-disable */
+    !(function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
+    v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
+    o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+    y=e.createElement(n);y.async=!0;y.src=`https://cdn.pendo.io/agent/static/${apiKey}/pendo.js`;
+    z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+    /* eslint-enable */
+  }
 
   identify(options = {}) {
-    if (canUseDOM) {
-      window.pendo.initialize(options)
-    }
-  },
+    window.pendo.initialize(options)
+  }
 
   trackEvent(options = {}) {
     const { event } = options
     delete options.event
-    if (canUseDOM) {
-      window.pendo.track(event, options)
-    }
-  },
+    window.pendo.track(event, options)
+  }
 
   willDestroy() {
-    if (canUseDOM) {
-      document.querySelector('script[src*="pendo.js"]').remove()
-      delete window.pendo
-    }
+    removeFromDOM('script[src*="pendo.js"]')
+    delete window.pendo
   }
-})
+}
