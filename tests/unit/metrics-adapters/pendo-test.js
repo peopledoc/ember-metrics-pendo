d2a4512fc52c8e2dbdc296eccffeb1229d5e0a4b
Guillaume Gérard <1322081+GreatWizard@users.noreply.github.com>
2021-07-20 11:01:46 +0200

    build: update to ember 3.24 (#112)

--- tests/unit/metrics-adapters/pendo-test.js
+++ tests/unit/metrics-adapters/pendo-test.js
@@ -1,65 +1,64 @@
-import { module, test } from "qunit"
-import { setupTest } from "ember-qunit"
-import sinon from "sinon"
-import Pendo from "ember-metrics-pendo/metrics-adapters/pendo"
+import { module, test } from 'qunit'
+import { setupTest } from 'ember-qunit'
+import sinon from 'sinon'
+import Pendo from 'ember-metrics-pendo/metrics-adapters/pendo'
 
 let config
 
-module("Unit | Metrics Adapter | pendo adapter", function(hooks) {
+module('Unit | Metrics Adapter | pendo adapter', function (hooks) {
   setupTest(hooks)
-  hooks.beforeEach(function() {
+  hooks.beforeEach(function () {
     config = {
-      apiKey: "123456789"
+      apiKey: '123456789',
     }
   })
 
-  test("#identify calls `pendo.initialize()` with the right arguments", function(assert) {
+  test('#identify calls `pendo.initialize()` with the right arguments', function (assert) {
     let adapter = Pendo.create({ config })
-    let stub = sinon.stub(window.pendo, "initialize").callsFake(() => {
+    let stub = sinon.stub(window.pendo, 'initialize').callsFake(() => {
       return true
     })
     adapter.identify({
       visitor: {
         id: 123,
-        role: "employee"
+        role: 'employee',
       },
       account: {
-        id: "def1abc2",
-        env: "development"
-      }
+        id: 'def1abc2',
+        env: 'development',
+      },
     })
     assert.ok(
       stub.calledWith({
         visitor: {
           id: 123,
-          role: "employee"
+          role: 'employee',
         },
         account: {
-          id: "def1abc2",
-          env: "development"
-        }
+          id: 'def1abc2',
+          env: 'development',
+        },
       }),
-      "it sends the correct arguments"
+      'it sends the correct arguments'
     )
   })
 
-  test("#trackEvent calls `pendo.track` with the right arguments", function(assert) {
+  test('#trackEvent calls `pendo.track` with the right arguments', function (assert) {
     let adapter = Pendo.create({ config })
-    let stub = sinon.stub(window.pendo, "track").callsFake(() => {
+    let stub = sinon.stub(window.pendo, 'track').callsFake(() => {
       return true
     })
     adapter.trackEvent({
       event: 'ClickedThing',
       prop1: 'ThingID',
-      prop2: 'ThingValue'
+      prop2: 'ThingValue',
     })
     assert.ok(
       stub.calledWith('ClickedThing', {
         prop1: 'ThingID',
-        prop2: 'ThingValue'
+        prop2: 'ThingValue',
       }),
-      "it sends the correct arguments"
+      'it sends the correct arguments'
     )
   })
-
 })
