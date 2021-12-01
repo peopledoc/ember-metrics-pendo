c83b42a7c7e7efb4f94d8facfc80578b964b3537
Guillaume Gérard <guillaume.gerard88@gmail.com>
2021-12-01 15:51:56 +0100

    feat: add deprecation warning

--- index.js
+++ index.js
@@ -2,4 +2,13 @@
 
 module.exports = {
   name: require('./package').name,
+
+  included(parent) {
+    this._super.included.apply(this, arguments)
+    console.warn(
+      `⚠️  DEPRECATION: [ember-metrics-pendo] Please use ember-metrics v1.4.0+ which includes the adapter for Pendo and remove ember-metrics-pendo from your dependencies.
+See https://github.com/adopted-ember-addons/ember-metrics for more details.`
+    )
+    return parent
+  },
 }
